8a0d569d0a724d9051f7e5942c1572cfe86bcf1c
Mark Olsson <mark.olsson@telldus.se>
2016-09-10 18:30:27 +0200

    Adds basic Apple Watch extension structure

---
+++ ios/TelldusLiveAppWatch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  TelldusLiveAppWatch Extension
+//
+//  Created by Mark Olsson on 2016-09-10.
+//  Copyright © 2016 Telldus Technologies AB. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
