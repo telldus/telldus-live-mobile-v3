c13259bc660901e03210ddcdfa8ab600dbf7d7d5
godspeedelbow <eelke@boezeman.com>
2017-05-08 10:29:42 +0200

    Added  @providesModule to Actions

--- js/App/Actions/Sensors.js
+++ js/App/Actions/Sensors.js
@@ -15,6 +15,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with Telldus Live! app.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @providesModule Actions/Sensors
  */
 
 'use strict';
