eb29d4433345014c96bfb1824da1ebe766943e64
Micke Prag <micke.prag@telldus.se>
2017-07-13 14:11:33 +0200

    Add flow definition file to be able to supress errors from node_modules

---
+++ flowdefs.js
@@ -0,0 +1,13 @@
+// This file is to be able to supress errors thrown by flow from modules in
+// node_modules. Until flow has a better way of handling this please ignore the
+// file throwing the error in .flowconfig and add a coresponding declaration here
+
+declare module 'AnimatedImplementation' {
+	declare var exports: any;
+}
+declare module 'MessageQueue' {
+	declare var exports: any;
+}
+declare module '../../local-cli/bundle/assetPathUtils' {
+	declare var exports: any;
+}
