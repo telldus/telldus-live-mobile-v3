bf67f026672616d4af58eb2c2a072cc842d2638c
Micke Prag <micke.prag@telldus.se>
2017-08-16 13:06:28 +0200

    Update application id to prepare for release

--- android/app/src/main/java/com/telldus/live/mobile/MainActivity.java
+++ android/app/src/main/java/com/telldus/live/mobile/MainActivity.java
@@ -17,7 +17,7 @@
  * along with Telldus Live! app.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.telldus.live.mobile.test;
+package com.telldus.live.mobile;
 
 import com.facebook.react.ReactActivity;
 import android.content.Intent;
