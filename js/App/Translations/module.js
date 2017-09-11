0a9bc4747197a8ce24869e95c7713240f00246fd
Fredrik Gullberg <fredrik.gullberg@telldus.se>
2017-09-11 14:51:12 +0200

    Add norwegian language support Changelog: Add Norwegian translation

--- js/App/Translations/module.js
+++ js/App/Translations/module.js
@@ -29,20 +29,20 @@ import {addLocaleData} from 'react-intl';
 // import cs from 'react-intl/locale-data/cs';
 import en from 'react-intl/locale-data/en';
 import fr from 'react-intl/locale-data/fr';
-// import nb from 'react-intl/locale-data/nb';
+import nb from 'react-intl/locale-data/nb';
 // import nl from 'react-intl/locale-data/nl';
 // import pl from 'react-intl/locale-data/pl';
 // import ru from 'react-intl/locale-data/ru';
 import sv from 'react-intl/locale-data/sv';
 // import th from 'react-intl/locale-data/th';
 
-addLocaleData([...en, ...fr, ...sv]);
+addLocaleData([...en, ...fr, ...nb, ...sv]);
 
 module.exports = {
 	// cs: require('./cs.json'),
 	en: require('./en.json'),
 	fr: require('./fr.json'),
-	// nb: require('./nb.json'),
+	nb: require('./nb.json'),
 	// nl: require('./nl.json'),
 	// pl: require('./pl.json'),
 	// ru: require('./ru.json'),
