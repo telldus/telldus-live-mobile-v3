e666beb0abb1ee817d65ca089e3dbaae5c803a6a
rimz <rimnesh.fernandez@gmail.com>
2017-10-02 12:06:50 +0530

    ignore flow errors from react-navigation module.

--- flowdefs.js
+++ flowdefs.js
@@ -1,11 +1,14 @@
 // This file is to be able to supress errors thrown by flow from modules in
 // node_modules. Until flow has a better way of handling this please ignore the
 // file throwing the error in .flowconfig and add a coresponding declaration here
-
+// @flow
 declare module 'AnimatedImplementation' {
 	declare var exports: any;
 }
 declare module 'MessageQueue' {
+	declare var exports: any;
+}
+declare module 'react-navigation' {
 	declare var exports: any;
 }
 declare module '../../local-cli/bundle/assetPathUtils' {
