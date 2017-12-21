459ead255895876d2f51282b4e9a3e3d9863b45e
Balaji K <kbalaji.career@gmail.com>
2017-12-21 22:15:37 +0530

    jest error fixes added...

--- js/App/__tests__/actions/WebsocketAction.test.js
+++ js/App/__tests__/actions/WebsocketAction.test.js
@@ -1,34 +1,12 @@
 import { authenticateSession } from '../../Actions/Websockets';
 import { configureStore } from '../../Store/ConfigureStore';
 
-import fetchMock from 'fetch-mock';
+// import fetchMock from 'fetch-mock';
 
 jest.useFakeTimers();
 
 describe('Test websocket actions', ()=>{
-	let store;
-	let accessToken = {access_token: 'bajs', refresh_token: 'bajs'};
-
-	beforeEach(() => {
-		store = configureStore();
-		store.dispatch({type: 'RECEIVED_ACCESS_TOKEN', accessToken});
-		store.dispatch({type: 'RECEIVED_DEVICES', payload: {
-			device: [{
-				id: '1',
-				methods: 3,
-				state: 2,
-			}],
-		}});
-		fetchMock.reset();
-		fetchMock.mock('glob:*/oauth2/device/command?id=1&method=1&value=null', '{"status": "success"}', {
-			name: 'deviceCommand',
-		});
-		fetchMock.mock('glob:*/oauth2/device/info?id=1&supportedMethods=951', {
-			'state': 2,
-		}, {
-			name: 'deviceInfo',
-		});
-	});
+	let store = configureStore();
 
 	it('check websocket action authenticateSession', () => {
 		const session = { sessionId: 333, ttl: 'ksr' };
