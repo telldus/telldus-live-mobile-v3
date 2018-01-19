d7b42ee067ac79fb79dd952fc1d8854746d0461e
rimz <rimnesh.fernandez@gmail.com>
2018-01-19 16:43:03 +0530

    Remove unnecessary code.

--- js/App/Components/PreLoginScreens/SubViews/RegisterForm.js
+++ js/App/Components/PreLoginScreens/SubViews/RegisterForm.js
@@ -179,9 +179,6 @@ class RegisterForm extends View {
 	validateEmail(email: string) {
 		let pattern = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
 		let emailValid = pattern.test(email);
-		if (!emailValid) {
-			this.showModal('Invalid Email');
-		}
 		return emailValid;
 	}
 
