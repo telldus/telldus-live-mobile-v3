0c7e4b9c344605ffb52fb6fce035086e76fb308a
rimz <rimnesh.fernandez@gmail.com>
2018-02-21 14:07:17 +0530

    Remove @providesModule - Drawer.

--- js/App/Components/TabViews/TabsView.android.js
+++ js/App/Components/TabViews/TabsView.android.js
@@ -40,7 +40,7 @@ import { getUserProfile } from '../../Reducers/User';
 import { syncWithServer, switchTab, addNewGateway } from '../../Actions';
 import TabViews from 'TabViews';
 import { TabNavigator } from 'react-navigation';
-import Drawer from 'Drawer';
+import Drawer from '../Drawer/Drawer';
 
 const messages = defineMessages({
 	menuIcon: {
