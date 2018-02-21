6e4714f8d19e3319045f6a2d6ca03f171a92060a
rimz <rimnesh.fernandez@gmail.com>
2018-02-21 12:26:59 +0530

    Remove @providesModule - Actions_Login.

--- js/App/Actions/Login.js
+++ js/App/Actions/Login.js
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU General Public License
  * along with Telldus Live! app.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @providesModule Actions_Login
  */
 
 // @flow
