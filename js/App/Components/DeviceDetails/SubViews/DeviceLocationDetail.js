9101a1e1117d8bbda7a58cddd12b052864deb642
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2018-02-27 17:32:45 +0530

    Fix design issues in gateways tab, Enable add location navigation - IOS.

--- js/App/Components/DeviceDetails/SubViews/DeviceLocationDetail.js
+++ js/App/Components/DeviceDetails/SubViews/DeviceLocationDetail.js
@@ -25,6 +25,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Text, View } from 'BaseComponents';
 import { StyleSheet, Image, TouchableWithoutFeedback } from 'react-native';
+import Theme from 'Theme';
 
 type Props = {
 	title?: any,
@@ -152,16 +153,7 @@ const styles = StyleSheet.create({
 		flexDirection: 'row',
 	},
 	shadow: {
-		borderRadius: 4,
-		backgroundColor: '#fff',
-		shadowColor: '#000000',
-		shadowOffset: {
-			width: 0,
-			height: 0,
-		},
-		shadowRadius: 1,
-		shadowOpacity: 1.0,
-		elevation: 2,
+		...Theme.Core.shadow,
 	},
 });
 
