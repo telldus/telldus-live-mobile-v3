ef96d34ea870903cdfa2b8bd5ae4ab1dd9a8e189
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2018-03-05 17:57:59 +0530

    Do not show navigational back button in the final screen - Add Location.

--- js/App/Components/AddLocation/AddLocation.ios.js
+++ js/App/Components/AddLocation/AddLocation.ios.js
@@ -88,7 +88,7 @@ const StackNavigatorConfig = {
 		let renderStackHeader = state.routeName !== 'LocationDetected';
 		if (renderStackHeader && isPortrait) {
 			return {
-				header: <NavigationHeader navigation={navigation} />,
+				header: <NavigationHeader navigation={navigation} showLeftIcon={state.routeName !== 'Success'}/>,
 			};
 		}
 		return {
