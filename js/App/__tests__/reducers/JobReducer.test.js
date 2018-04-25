fa9dac38949110eeb0b57cb6b90266431312f00c
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2018-04-25 18:20:16 +0530

    Update initial state of Job reducer in unit test file - JobReducer.

--- js/App/__tests__/reducers/JobReducer.test.js
+++ js/App/__tests__/reducers/JobReducer.test.js
@@ -2,7 +2,7 @@ import {parseJobsForListView} from '../../Reducers/Jobs.js';
 
 jest.useFakeTimers();
 
-const initialState = { sectionIds: [], sections: {} };
+const initialState = { sectionIds: [], sections: {'0': [], '1': [], '2': [], '3': [], '4': [], '5': [], '6': []} };
 
 describe('Test jobs reducers', ()=>{
 
