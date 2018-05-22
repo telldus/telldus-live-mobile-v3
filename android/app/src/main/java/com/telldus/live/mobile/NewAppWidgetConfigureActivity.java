51dc9898dd77392d35288b2cd9719c75e9ceb7df
Balaji K <kbalaji.career@gmail.com>
2018-05-22 13:14:08 +0530

    Widget Ui alignment issues fixed...

--- android/app/src/main/java/com/telldus/live/mobile/NewAppWidgetConfigureActivity.java
+++ android/app/src/main/java/com/telldus/live/mobile/NewAppWidgetConfigureActivity.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.graphics.Typeface;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -357,7 +358,10 @@ public class NewAppWidgetConfigureActivity extends Activity {
                 ad = builder.show();//   builder.show();
             }
         });
-
+        Typeface title = Typeface.createFromAsset(getAssets(),"fonts/RobotoLight.ttf");
+                Typeface subtitle = Typeface.createFromAsset(getAssets(),"fonts/RobotoLight.ttf");
+                textTest.setTypeface(title);
+                chooseSetting.setTypeface(subtitle);
 
     }
 
