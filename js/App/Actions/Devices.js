8a28a7b9656e2bf26b0099dfcc9abf52b45b7a16
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2018-05-29 12:28:25 +0530

    Update device action 'deviceSetState' to include local control of the devices. See #378.

--- js/App/Actions/Devices.js
+++ js/App/Actions/Devices.js
@@ -21,10 +21,68 @@
 // @flow
 
 'use strict';
+
+import { format } from 'url';
+let forge = require('node-forge');
+
+import type { ThunkAction } from './Types';
+import { LocalApi, getRSAKey } from '../Lib';
 // Device actions that are shared by both Web and Mobile.
 import { actions } from 'live-shared-data';
 const { Devices } = actions;
+const { deviceSetState: deviceSetStateShared, ...otherActions } = Devices;
 
 module.exports = {
-	...Devices,
+	...otherActions,
+	deviceSetState,
 };
+
+function deviceSetState(deviceId: number, state: number, stateValue: number | null = null): ThunkAction {
+	return (dispatch: Function, getState: Function): any => {
+		const { gateways, devices } = getState();
+		const { clientId } = devices.byId[deviceId];
+		const { localKey } = gateways.byId[clientId];
+		const { address, key } = localKey;
+
+		if (address && key) {
+			getRSAKey(false, ({ pemPvt }: Object): ThunkAction => {
+				const privateKey = forge.pki.privateKeyFromPem(pemPvt);
+				const decoded64 = forge.util.decode64(key);
+				const token = privateKey.decrypt(decoded64, 'RSA-OAEP', {
+					md: forge.md.sha256.create(),
+				});
+				// console.log('TEST IN token', token);
+
+				if (token) {
+					const url = format({
+						pathname: '/device/command',
+						query: {
+							id: deviceId,
+							method: state,
+							value: stateValue,
+						},
+					});
+					const payload = {
+						address,
+						url,
+						requestParams: {
+							method: 'GET',
+						},
+						token,
+					};
+					return LocalApi(payload).then((response: Object): any => {
+						// console.log('TEST response', response);
+						return response;
+					}).catch((err: Object): any => {
+						console.log('err', err);
+						return dispatch(deviceSetStateShared(deviceId, state, stateValue));
+					});
+				}
+				// console.log('TEST gateways.byId[clientId]', gateways.byId[clientId]);
+				return dispatch(deviceSetStateShared(deviceId, state, stateValue));
+			});
+		} else {
+			return dispatch(deviceSetStateShared(deviceId, state, stateValue));
+		}
+	};
+}
