9c40bfcbb4a0e4cd5c8b040c6f0c97f52403cd9e
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2018-06-22 12:43:11 +0530

    Upgrade dependency 'external-editor'
    v3 includes a breaking change, which is named import, so
    making the change in the code.

--- js/Utils/release.js
+++ js/Utils/release.js
@@ -1,6 +1,27 @@
+/**
+ * Copyright 2016-present Telldus Technologies AB.
+ *
+ * This file is part of the Telldus Live! app.
+ *
+ * Telldus Live! app is free : you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Telldus Live! app is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Telldus Live! app.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+import { edit } from 'external-editor';
+
 let exec = require('child-process-promise').exec;
 let fs = require('fs');
-let ExternalEditor = require('external-editor');
 let prompt = require('prompt');
 
 let changelog = '';
@@ -52,7 +73,7 @@ exec('git diff-index --quiet HEAD --')
 	})
 	.then(() => {
 		// Let the user edit the changelog before commiting
-		changelog = ExternalEditor.edit(changelog);
+		changelog = edit(changelog);
 	})
 	.then(() => {
 		// Generate changelog for Android
