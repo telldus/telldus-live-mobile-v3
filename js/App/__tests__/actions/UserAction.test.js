395685f5c907301f20f4ba2a46aa8d3b9f6ea796
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2018-06-27 18:13:40 +0530

    Fix failing test - UserAction

--- js/App/__tests__/actions/UserAction.test.js
+++ js/App/__tests__/actions/UserAction.test.js
@@ -1,15 +1,26 @@
 import { unregisterPushToken } from '../../Actions/User';
 import { configureStore } from '../../Store/ConfigureStore';
 import { format } from 'url';
-// import fetchMock from 'fetch-mock';
+import fetchMock from 'fetch-mock';
 
 jest.useFakeTimers();
 
 describe('Test User actions', ()=>{
-	let store = configureStore();
+	let store;
+	let accessToken = {access_token: 'bajs', refresh_token: 'bajs'};
+	let token = 'token';
+
+	beforeEach(() => {
+		store = configureStore();
+		store.dispatch({type: 'RECEIVED_ACCESS_TOKEN', accessToken});
+		fetchMock.mock('glob:*/oauth2/user/unregisterPushToken?token=token', '{"status": "success"}', {
+			name: 'unregisterPushToken',
+			overwriteRoutes: true,
+		});
+	});
 
 	it('check user unregisterPushToken', () => {
-		const token = 'token';
+		store = configureStore();
 		const url = format({ pathname: '/user/unregisterPushToken',	query: { token }});
 		const payload = { url, requestParams: {	method: 'GET'}};
 		return store.dispatch(unregisterPushToken(token))
@@ -19,6 +30,7 @@ describe('Test User actions', ()=>{
 			})
 			.then(() => {
 				expect(store.getState().user.pushTokenRegistered).toBe(false);
+				expect(fetchMock.calls('unregisterPushToken').length).toBe(1);
 			});
 	});
 
