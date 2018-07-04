94aa4412d05d6f5a1ed6110f4d756a503b1a1482
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2018-07-04 17:21:57 +0530

    Fix lint errors

--- js/App/Components/AppNavigatorRenderer.android.js
+++ js/App/Components/AppNavigatorRenderer.android.js
@@ -22,7 +22,6 @@
 'use strict';
 
 import React from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { NetInfo } from 'react-native';
 import Toast from 'react-native-simple-toast';
