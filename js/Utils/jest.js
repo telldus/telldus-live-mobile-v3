34ffa690de016d0c969e5f283b5e09aaeed89e24
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2018-07-20 18:48:13 +0530

    Fix jest test suite issue
    Mock dependency 'react-native-orientation-locker'

--- js/Utils/jest.js
+++ js/Utils/jest.js
@@ -9,3 +9,14 @@ jest.mock('react-native-fabric', () => {
 		},
 	};
 });
+
+jest.mock('react-native-orientation-locker', () => {
+	return {
+		addEventListener: jest.fn(),
+		removeEventListener: jest.fn(),
+		lockToPortrait: jest.fn(),
+		lockToLandscapeLeft: jest.fn(),
+		lockToLandscapeRight: jest.fn(),
+		unlockAllOrientations: jest.fn(),
+	};
+});
