12c795175a9d938c21c9a7823bfdf311bfcbc682
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2018-09-20 16:46:52 +0530

    Fix bug in redux persist store migration code
    It is wrong to return promise with new state from each 'if' condition/
    stage of migration, this will result only one change/migration to happen
    and return new state and thereby cause crash due to other excluded
    migrations(at least on first launch).
    
    With this commit, during each stage redux state is modified and once
    all migrations are updated, return the new state.

--- js/App/Store/migrations.js
+++ js/App/Store/migrations.js
@@ -23,26 +23,34 @@
 'use strict';
 
 export default function migrations(state: Object = {}): Promise<any> {
-	const { gateways } = state;
-	if (gateways) {
-		const newGateways = insertLocalKeyAttribute(gateways);
-		return Promise.resolve({
-			...state,
-			gateways: newGateways,
-		});
-	}
-	const { tabs, ...newState } = state;
+	const { tabs, ...withOutTabs } = state;
+
+	let newState = state;
+	// Remove redux store object 'tabs' if present.
 	if (tabs) {
-		return Promise.resolve(newState);
+		newState = withOutTabs;
 	}
-	const { App } = newState;
+
+	const { App, ...withOutApp } = newState;
+	// Rename redux store object 'App' to 'app'.
 	if (App) {
-		return Promise.resolve({
-			...newState,
-			app: state.App,
-		});
+		newState = {
+			...withOutApp,
+			app: App,
+		};
 	}
-	return Promise.resolve(state);
+
+	const { gateways } = newState;
+	if (gateways) {
+		// Insert the attribute/property 'localKey' to each gateways object if not already present.
+		const newGateways = insertLocalKeyAttribute(gateways);
+		newState = {
+			...newState,
+			gateways: newGateways,
+		};
+	}
+
+	return Promise.resolve(newState);
 }
 
 
