e04278c0d37aa8d024ee6467e475299b96b9e29e
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2018-09-28 15:20:47 +0530

    Add method 'disableAllWidgets' to native WidgetModule
    Expects one argument of string type, which is the message to be
    displayed on disabled widgets. This can be used at cases like
    log out from app, and since widgets cannot be removed from home
    screen programatically, using this method it can be disabled and
    any helpful message can be shown.

--- android/app/src/main/java/com/telldus/live/mobile/WidgetModule.java
+++ android/app/src/main/java/com/telldus/live/mobile/WidgetModule.java
@@ -6,9 +6,19 @@ import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.util.Log;
+import android.widget.RemoteViews;
+import android.view.View;
+
 import java.lang.System;
 import java.lang.String;
-import android.util.Log;
+
+
+import com.telldus.live.mobile.NewAppWidget;
+import com.telldus.live.mobile.NewOnOffWidget;
+import com.telldus.live.mobile.NewSensorWidget;
 
 import com.telldus.live.mobile.Database.PrefManager;
 
@@ -27,18 +37,32 @@ public class WidgetModule extends ReactContextBaseJavaModule {
 
   @ReactMethod
   public void configureWidgetAuthData(String accessToken, String refreshToken, String expiresIn, String clientId, String clientSecret) {
-    prefManager=new PrefManager(getReactApplicationContext());
+    prefManager = new PrefManager(getReactApplicationContext());
 
     prefManager.timeStampAccessToken(expiresIn);
     prefManager.AccessTokenDetails(accessToken, expiresIn);
     prefManager.infoAccessToken(clientId, clientSecret, refreshToken);
   }
 
-
   @ReactMethod
   public void configureWidgetSessionData(String sessionId) {
-    prefManager=new PrefManager(getReactApplicationContext());
+    prefManager = new PrefManager(getReactApplicationContext());
 
     prefManager.saveSessionID(sessionId);
+  }
+
+  @ReactMethod
+  public void disableAllWidgets(String message) {
+    RemoteViews widgetView = new RemoteViews(getReactApplicationContext().getPackageName(), R.layout.new_app_widget);
+    
+    widgetView.removeAllViews(R.id.widget_content_cover);
+    widgetView.setTextViewText(R.id.txtWidgetTitle, message);
+
+    prefManager = new PrefManager(getReactApplicationContext());
+    prefManager.clear();
+
+    AppWidgetManager.getInstance(getReactApplicationContext()).updateAppWidget(new ComponentName(getReactApplicationContext(), NewAppWidget.class), widgetView);
+    AppWidgetManager.getInstance(getReactApplicationContext()).updateAppWidget(new ComponentName(getReactApplicationContext(), NewOnOffWidget.class), widgetView);
+    AppWidgetManager.getInstance(getReactApplicationContext()).updateAppWidget(new ComponentName(getReactApplicationContext(), NewSensorWidget.class), widgetView);
   }
 }
