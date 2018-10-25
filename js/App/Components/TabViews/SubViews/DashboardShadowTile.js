6c729709cb192e1f0a810c2362f6a8a956825f73
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2018-10-25 16:40:44 +0530

    Add style attribute 'backgroundColor' on View with shadow - DB
    Fix react warning.

--- js/App/Components/TabViews/SubViews/DashboardShadowTile.js
+++ js/App/Components/TabViews/SubViews/DashboardShadowTile.js
@@ -88,7 +88,7 @@ class DashboardShadowTile extends View {
 			<View
 				accessible={true}
 				accessibilityLabel={accessibilityLabel}
-				style={[this.props.style, Theme.Core.shadow, {elevation: 3}]}>
+				style={[this.props.style, Theme.Core.shadow, {elevation: 3, backgroundColor: '#fff'}]}>
 				<View style={{
 					flexDirection: 'column',
 					borderRadius: 2,
