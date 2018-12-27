fb0221c256c3eb1995bfcf4e957d78a02f6ac66f
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2018-12-27 12:13:27 +0530

    Update navigator 'AddLocation'!
    Issue and need for it is explained in this commit
    6b910a9a14cb80508361230f6540b4f6e799870f

--- js/App/Components/Location/AddLocation/AddLocationNavigator.js
+++ js/App/Components/Location/AddLocation/AddLocationNavigator.js
@@ -40,8 +40,8 @@ const initialRouteName = 'LocationDetected';
 
 type renderContainer = (Object) => Object;
 
-const renderAddLocationContainer = (navigation: Object, screenProps: Object): renderContainer => (Component: Object): Object => (
-	<AddLocationContainer navigation={navigation} screenProps={screenProps}>
+const renderAddLocationContainer = (navigation: Object, screenProps: Object): renderContainer => (Component: Object, ScreenName: string): Object => (
+	<AddLocationContainer navigation={navigation} screenProps={screenProps} ScreenName={ScreenName}>
 		<Component/>
 	</AddLocationContainer>
 );
@@ -49,28 +49,28 @@ const renderAddLocationContainer = (navigation: Object, screenProps: Object): re
 
 const RouteConfigs = {
 	LocationDetected: {
-		screen: ({ navigation, screenProps }: Object): Object => renderAddLocationContainer(navigation, screenProps)(LocationDetected),
+		screen: ({ navigation, screenProps }: Object): Object => renderAddLocationContainer(navigation, screenProps)(LocationDetected, 'LocationDetected'),
 	},
 	LocationActivationManual: {
-		screen: ({ navigation, screenProps }: Object): Object => renderAddLocationContainer(navigation, screenProps)(LocationActivationManual),
+		screen: ({ navigation, screenProps }: Object): Object => renderAddLocationContainer(navigation, screenProps)(LocationActivationManual, 'LocationActivationManual'),
 	},
 	LocationName: {
-		screen: ({ navigation, screenProps }: Object): Object => renderAddLocationContainer(navigation, screenProps)(LocationName),
+		screen: ({ navigation, screenProps }: Object): Object => renderAddLocationContainer(navigation, screenProps)(LocationName, 'LocationName'),
 	},
 	TimeZoneContinent: {
-		screen: ({ navigation, screenProps }: Object): Object => renderAddLocationContainer(navigation, screenProps)(TimeZoneContinent),
+		screen: ({ navigation, screenProps }: Object): Object => renderAddLocationContainer(navigation, screenProps)(TimeZoneContinent, 'TimeZoneContinent'),
 	},
 	TimeZoneCity: {
-		screen: ({ navigation, screenProps }: Object): Object => renderAddLocationContainer(navigation, screenProps)(TimeZoneCity),
+		screen: ({ navigation, screenProps }: Object): Object => renderAddLocationContainer(navigation, screenProps)(TimeZoneCity, 'TimeZoneCity'),
 	},
 	TimeZone: {
-		screen: ({ navigation, screenProps }: Object): Object => renderAddLocationContainer(navigation, screenProps)(TimeZone),
+		screen: ({ navigation, screenProps }: Object): Object => renderAddLocationContainer(navigation, screenProps)(TimeZone, 'TimeZone'),
 	},
 	Position: {
-		screen: ({ navigation, screenProps }: Object): Object => renderAddLocationContainer(navigation, screenProps)(Position),
+		screen: ({ navigation, screenProps }: Object): Object => renderAddLocationContainer(navigation, screenProps)(Position, 'Position'),
 	},
 	Success: {
-		screen: ({ navigation, screenProps }: Object): Object => renderAddLocationContainer(navigation, screenProps)(Success),
+		screen: ({ navigation, screenProps }: Object): Object => renderAddLocationContainer(navigation, screenProps)(Success, 'Success'),
 	},
 };
 
