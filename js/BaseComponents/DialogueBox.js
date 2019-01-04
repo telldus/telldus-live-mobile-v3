011cbc1ec66ca273b7fb55f8a608b4a2942b6b08
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2019-01-04 22:35:30 +0530

    Update component DialogueBox - expose new props
    following are newly added:
    capitalizeHeader, negTextColor and posTextColor.

--- js/BaseComponents/DialogueBox.js
+++ js/BaseComponents/DialogueBox.js
@@ -34,6 +34,7 @@ import DialogueHeader from './DialogueHeader';
 
 import capitalize from '../App/Lib/capitalize';
 import i18n from '../App/Translations/common';
+import Theme from '../App/Theme';
 
 
 type Props = {
@@ -60,6 +61,9 @@ type Props = {
 	style?: number | Object | Array<any>,
 	showHeader?: boolean,
 	backdropColor?: string,
+	capitalizeHeader?: boolean,
+	negTextColor?: string,
+	posTextColor?: string,
 };
 
 type defaultProps = {
@@ -70,6 +74,9 @@ type defaultProps = {
 	exitDuration: number,
 	backdropOpacity: number,
 	showHeader: boolean,
+	capitalizeHeader: boolean,
+	negTextColor: string,
+	posTextColor: string,
 };
 
 class DialogueBox extends Component<Props, null> {
@@ -85,6 +92,9 @@ class DialogueBox extends Component<Props, null> {
 		backdropColor: '#000',
 		backdropOpacity: 0.60,
 		showHeader: true,
+		capitalizeHeader: true,
+		negTextColor: '#6B6969',
+		posTextColor: Theme.Core.brandSecondary,
 	}
 
 	renderHeader: (Object) => void;
@@ -164,25 +174,26 @@ class DialogueBox extends Component<Props, null> {
 		}
 	}
 
-	dialogueImageHeader({ showIconOnHeader, header, onPressHeader, onPressHeaderIcon, dialogueHeaderStyle, notificationModalHeaderText }: Object): Object {
+	dialogueImageHeader({ showIconOnHeader, header, capitalizeHeader, onPressHeader, onPressHeaderIcon, dialogueHeaderStyle, notificationModalHeaderText }: Object): Object {
 		return (
 			<DialogueHeader
-				headerText={typeof header === 'string' ? capitalize(header) : header}
+				headerText={typeof header === 'string' && capitalizeHeader ? capitalize(header) : header}
 				showIcon={showIconOnHeader}
 				onPressHeader={onPressHeader}
 				headerStyle={dialogueHeaderStyle}
 				onPressIcon={onPressHeaderIcon}
-				textStyle={notificationModalHeaderText}/>
+				textStyle={notificationModalHeaderText}
+				shouldCapitalize={capitalizeHeader}/>
 		);
 	}
 
 	renderHeader(styles: Object): any {
-		let { header, showIconOnHeader, imageHeader, onPressHeader, onPressHeaderIcon } = this.props;
+		let { header, capitalizeHeader, showIconOnHeader, imageHeader, onPressHeader, onPressHeaderIcon } = this.props;
 		const { notificationModalHeader, dialogueHeaderStyle, notificationModalHeaderText } = styles;
 
 		if (imageHeader) {
 			return this.dialogueImageHeader({
-				showIconOnHeader, header, onPressHeader, onPressHeaderIcon,
+				showIconOnHeader, header, capitalizeHeader, onPressHeader, onPressHeaderIcon,
 				dialogueHeaderStyle, notificationModalHeaderText});
 		} else if (header && typeof header === 'object') {
 			return (
@@ -192,7 +203,7 @@ class DialogueBox extends Component<Props, null> {
 			header = this.defaultHeader;
 		}
 
-		header = typeof header === 'string' ? capitalize(header) : header;
+		header = typeof header === 'string' && capitalizeHeader ? capitalize(header) : header;
 		return (
 			<View style={notificationModalHeader}>
 				<Text style={notificationModalHeaderText}>
@@ -313,7 +324,7 @@ class DialogueBox extends Component<Props, null> {
 	}
 
 	getStyles(): Object {
-		const { appLayout } = this.props;
+		const { appLayout, negTextColor, posTextColor } = this.props;
 		const { width, height } = appLayout;
 		const isPortrait = height > width;
 		const deviceWidth = isPortrait ? width : height;
@@ -374,12 +385,12 @@ class DialogueBox extends Component<Props, null> {
 				paddingLeft: 5,
 			},
 			notificationModalFooterNegativeText: {
-				color: '#6B6969',
+				color: negTextColor,
 				fontSize,
 				fontWeight: 'bold',
 			},
 			notificationModalFooterPositiveText: {
-				color: '#e26901',
+				color: posTextColor,
 				fontSize,
 				fontWeight: 'bold',
 			},
