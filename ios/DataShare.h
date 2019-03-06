2b1f421e99d6a5ba6d142e2f8e3152928b56f75f
Sunil <sunil.zalavadiya1989@gmail.com>
2019-03-06 17:06:02 +0530

    Managing hide and view widget via userDefault from xcode side  - add native method for saving userDefault in xcode  - Calling this method in Dashboard and widget class from react side.

---
+++ ios/DataShare.h
@@ -0,0 +1,17 @@
+//
+//  DataShare.h
+//  TelldusLiveApp
+//
+//  Created by micra on 06/03/19.
+//  Copyright © 2019 Telldus Technologies AB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DataShare : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/DataShare.m
@@ -0,0 +1,13 @@
+//
+//  DataShare.m
+//  TelldusLiveApp
+//
+//  Created by micra on 06/03/19.
+//  Copyright © 2019 Telldus Technologies AB. All rights reserved.
+//
+
+#import "DataShare.h"
+
+@implementation DataShare
+
+@end
