d978c09731859c9bb561aa3d9606492833f5b66f
Sunil <sunil.zalavadiya1989@gmail.com>
2019-03-23 09:58:12 +0530

    Fix Flow type - Remove disable flow rule and add return type to each method

--- js/App/Components/TabViews/SubViews/Device/ModalRGB.js
+++ js/App/Components/TabViews/SubViews/Device/ModalRGB.js
@@ -1,5 +1,3 @@
-/* eslint-disable flowtype/require-parameter-type */
-/* eslint-disable flowtype/require-return-type */
 /**
  * Copyright 2016-present Telldus Technologies AB.
  *
@@ -20,10 +18,11 @@
  */
 
 // @flow
-/* eslint-disable no-mixed-spaces-and-tabs */
 import React, { Fragment } from 'react';
-import { Modal, BackHandler, Text, Animated, PanResponder,
-	 TouchableWithoutFeedback, ImageBackground } from 'react-native';
+import {
+	Modal, BackHandler, Text, Animated, PanResponder,
+	TouchableWithoutFeedback, ImageBackground,
+} from 'react-native';
 import { SafeAreaView } from 'react-navigation';
 import { connect } from 'react-redux';
 import Slider from 'react-native-slider';
@@ -53,15 +52,15 @@ type State = {
 };
 
 class ModalRGB extends View<Props, State> {
-    props: Props;
-    state: State;
+	props: Props;
+	state: State;
 
-    state = {
-    	sliderValue: 10,
-    	pixelColor: [255, 73, 51],
-    	width: 1,
-    	height: 1,
-    };
+	state = {
+		sliderValue: 10,
+		pixelColor: [255, 73, 51],
+		width: 1,
+		height: 1,
+	};
 
 	animations = {
 		handlePosition: new Animated.ValueXY({ x: 0, y: 0 }),
@@ -70,21 +69,21 @@ class ModalRGB extends View<Props, State> {
 	panResponders = {
 		handle: PanResponder.create({
 			onMoveShouldSetResponderCapture: () => true,
-			onStartShouldSetPanResponder: (e, gestureState) => true,
+			onStartShouldSetPanResponder: (e: Object, gestureState: Object) => true,
 			onMoveShouldSetPanResponderCapture: () => true,
 			onResponderTerminationRequest: () => false,
-		  onPanResponderGrant: (e, gestureState) => {
-			  this.animations.handlePosition.setOffset({ x: this.lastHandlePosition.x, y: this.lastHandlePosition.y });
-			  this.animations.handlePosition.setValue({ x: 0, y: 0 });
-		  },
-		  onPanResponderMove: (e, gestureState) => {
+			onPanResponderGrant: (e: Object, gestureState: Object) => {
+				this.animations.handlePosition.setOffset({ x: this.lastHandlePosition.x, y: this.lastHandlePosition.y });
+				this.animations.handlePosition.setValue({ x: 0, y: 0 });
+			},
+			onPanResponderMove: (e: Object, gestureState: Object) => {
 				getPixelRGBA('rgbpicker.png', e.nativeEvent.pageX, e.nativeEvent.pageY)
-					.then(color => this.setState({ pixelColor: color })) // [243, 123, 0]
-					.catch(err => {});
-
-				return this.animations.handlePosition.setValue({ x: gestureState.dx, y: gestureState.dy });
-		  },
-		  onPanResponderRelease: (e, { vx, vy }) => this.onRelease(),
+					.then((color: Array) => {
+						this.setState({ pixelColor: color });
+				 });
+				 return this.animations.handlePosition.setValue({ x: gestureState.dx, y: gestureState.dy });
+			},
+			onPanResponderRelease: () => this.onRelease(),
 		}),
 	}
 
@@ -101,27 +100,26 @@ class ModalRGB extends View<Props, State> {
 	}
 
 	componentWillMount() {
-		this.animations.handlePosition.x.addListener(({ value }) => this.lastHandlePosition.x = value);
-    	this.animations.handlePosition.y.addListener(({ value }) => this.lastHandlePosition.y = value);
-    	BackHandler.addEventListener('hardwareBackPress', this.handleBackButtonClick);
+		this.animations.handlePosition.x.addListener(({ value }: { value: number } ) => this.lastHandlePosition.x = value);
+		this.animations.handlePosition.y.addListener(({ value }: { value: number }) => this.lastHandlePosition.y = value);
+		BackHandler.addEventListener('hardwareBackPress', this.handleBackButtonClick);
 	}
 
 	componentWillUnmount() {
-    	BackHandler.removeEventListener('hardwareBackPress', this.handleBackButtonClick);
+		BackHandler.removeEventListener('hardwareBackPress', this.handleBackButtonClick);
 	}
 
-    handleBackButtonClick = () => {
-    	const { openModal } = this.props;
-    	openModal();
-    }
+	handleBackButtonClick = () => {
+		const { openModal } = this.props;
+		openModal();
+	}
 
-	  onLayout = ({ nativeEvent }: { nativeEvent: { layout: Object }}) => {
-	  	const { width, height } = nativeEvent.layout;
-    	this.setState({ width, height });
-	  }
+	onLayout = ({ nativeEvent }: { nativeEvent: { layout: Object } }) => {
+		const { width, height } = nativeEvent.layout;
+		this.setState({ width, height });
+	}
 
-	// eslint-disable-next-line flowtype/require-return-type
-	renderBanner = () => {
+	renderBanner(): Object {
 		const { circle, txtLbl } = styles;
 		return (
 			<Poster>
@@ -135,15 +133,14 @@ class ModalRGB extends View<Props, State> {
 		);
 	}
 
-	// eslint-disable-next-line flowtype/require-return-type
-	renderColorPicker = () => {
+	renderColorPicker(): Object {
 		const { pixelColor } = this.state;
 		return (
-			<Animated.View style={[ styles.shadowCard, { flex: 1, alignItems: 'center' }]} >
+			<Animated.View style={[styles.shadowCard, { flex: 1, alignItems: 'center' }]} >
 				<TouchableWithoutFeedback>
 					<ImageBackground
 						imageStyle={{ borderRadius: 2 }}
-						style={{ height: '100%', width: '100%'}}
+						style={{ height: '100%', width: '100%' }}
 						source={require('../../img/rgbpicker.png')}
 						onLayout={this.onLayout}
 					>
@@ -164,8 +161,7 @@ class ModalRGB extends View<Props, State> {
 		this.setState({ sliderValue: value });
 	}
 
-	// eslint-disable-next-line flowtype/require-return-type
-	renderSlider = () => {
+	renderSlider(): Object {
 		const { sliderValue } = this.state;
 		const color = Theme.Core.brandSecondary;
 		return (
@@ -183,28 +179,28 @@ class ModalRGB extends View<Props, State> {
 		);
 	}
 
-	render() {
-		  const { isModelRGB } = this.props;
-    	return (
-    		<Modal
-    			animationType="slide"
-    			visible={isModelRGB}
-    			supportedOrientations={['portrait', 'landscape']}
-    			onRequestClose={this.handleBackButtonClick}
-    		>
-    			<Fragment>
-    				<SafeAreaView style={{ flex: 0, backgroundColor: Theme.Core.brandPrimary }} />
-    				<SafeAreaView style={{ flex: 1, backgroundColor: '#ECEBEB' }}>
-    					<NavigationHeader leftIcon="close" isFromModal={true} onClose={this.handleBackButtonClick} />
-    					{this.renderBanner()}
-    					<View style={{ height: 300 }}>
-    						{this.renderColorPicker()}
-    					</View>
+	render(): Object {
+		const { isModelRGB } = this.props;
+		return (
+			<Modal
+				animationType="slide"
+				visible={isModelRGB}
+				supportedOrientations={['portrait', 'landscape']}
+				onRequestClose={this.handleBackButtonClick}
+			>
+				<Fragment>
+					<SafeAreaView style={{ flex: 0, backgroundColor: Theme.Core.brandPrimary }} />
+					<SafeAreaView style={{ flex: 1, backgroundColor: '#ECEBEB' }}>
+						<NavigationHeader leftIcon="close" isFromModal={true} onClose={this.handleBackButtonClick} />
+						{this.renderBanner()}
+						<View style={{ height: 300 }}>
+							{this.renderColorPicker()}
+						</View>
 						{this.renderSlider()}
-    				</SafeAreaView>
-    			</Fragment>
-    		</Modal>
-    	);
+					</SafeAreaView>
+				</Fragment>
+			</Modal>
+		);
 	}
 }
 
