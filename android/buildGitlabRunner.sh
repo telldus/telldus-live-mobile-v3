2a81c46ddd5a0b1bc312cfa8d025504af9f370a9
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2019-05-15 10:09:35 +0530

    Changes missed in the previous commit

--- android/buildGitlabRunner.sh
+++ android/buildGitlabRunner.sh
@@ -26,7 +26,6 @@ module.exports = {
 EOF
 
 cat > gradle.properties <<EOF
-android.enableAapt2=false
 TELLDUS_REACT_NATIVE_LOCAL_STORE_FILE=../android-signing/telldus.keystore
 TELLDUS_REACT_NATIVE_LOCAL_KEY_ALIAS=telldus
 TELLDUS_REACT_NATIVE_LOCAL_STORE_PASSWORD=${ANDROID_STORE_PASSWORD}
