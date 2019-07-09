f1c6e6410b4cff4abddfae75698c826851111503
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2019-07-09 13:52:32 +0530

    Do not show offset for a time schedule(after update)
    Fixes #843.

--- js/App/Components/Schedule/Time.js
+++ js/App/Components/Schedule/Time.js
@@ -179,6 +179,7 @@ export default class Time extends View<null, Props, State> {
 		} else {
 			time.hour = date.getHours();
 			time.minute = date.getMinutes();
+			time.offset = 0;
 		}
 
 		actions.selectTime(selectedType, time);
