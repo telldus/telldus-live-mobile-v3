1265c137abb77afaafe5ae1ee31d26008919b679
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2019-09-05 21:36:07 +0530

    Add new component UpgradePremiumButton
    See #156.

---
+++ js/App/Components/Premium/SubViews/UpgradePremiumButton.js
@@ -0,0 +1,71 @@
+/**
+ * Copyright 2016-present Telldus Technologies AB.
+ *
+ * This file is part of the Telldus Live! app.
+ *
+ * Telldus Live! app is free : you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Telldus Live! app is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Telldus Live! app.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+// @flow
+
+'use strict';
+
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import {
+	TouchableButton,
+} from '../../../../BaseComponents';
+
+const UpgradePremiumButton = (props: Object): Object => {
+	const { buttonAccessibleProp = true, navigation } = props;
+
+	const { app: { layout } } = useSelector((state: Object): Object => state);
+
+	const {
+		fontSize,
+	} = getStyles(layout);
+	function onPress() {
+		navigation.navigate({
+			routeName: 'PremiumBenefitsScreen',
+			key: 'PremiumBenefitsScreen',
+		});
+	}
+
+	return (
+		<TouchableButton
+			onPress={onPress}
+			text={'View premium benefits'}
+			accessibilityLabel={'View premium benefits'}
+			accessible={buttonAccessibleProp}
+			style={{
+				marginTop: fontSize / 2,
+			}}
+		/>
+	);
+};
+
+const getStyles = (appLayout: Object): Object => {
+	const { height, width } = appLayout;
+	const isPortrait = height > width;
+	const deviceWidth = isPortrait ? width : height;
+	const fontSize = Math.floor(deviceWidth * 0.045);
+
+	return {
+		fontSize,
+	};
+};
+
+export default UpgradePremiumButton;
