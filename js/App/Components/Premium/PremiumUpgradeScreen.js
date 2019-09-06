bb1e4ca2db5c2b92fdfb6f3eda171701970ee5fc
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2019-09-06 10:49:42 +0530

    Add new component PremiumUpgradeScreen
    See #156.

---
+++ js/App/Components/Premium/PremiumUpgradeScreen.js
@@ -0,0 +1,91 @@
+/**
+ * Copyright 2016-present Telldus Technologies AB.
+ *
+ * This file is part of the Telldus Live! app.
+ *
+ * Telldus Live! app is free : you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Telldus Live! app is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Telldus Live! app.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+// @flow
+
+'use strict';
+
+import React from 'react';
+import { ScrollView } from 'react-native';
+import { useSelector } from 'react-redux';
+
+import {
+	View,
+	NavigationHeaderPoster,
+	Text,
+	IconTelldus,
+} from '../../../BaseComponents';
+
+import Theme from '../../Theme';
+
+const PremiumUpgradeScreen = (props: Object): Object => {
+	const { navigation, screenProps } = props;
+	const { app: {layout} } = useSelector((state: Object): Object => state);
+	const {
+		container,
+		body,
+	} = getStyles(layout);
+
+	return (
+		<View style={container}>
+			<NavigationHeaderPoster
+				h1={'Premium access'} h2={'Get more features & benefits'}
+				align={'right'}
+				showLeftIcon={true}
+				leftIcon={'close'}
+				navigation={navigation}
+				{...screenProps}/>
+			<ScrollView style={{flex: 1}} contentContainerStyle={{ flexGrow: 1 }}>
+				<View style={body} >
+					<IconTelldus icon={'premium'}/>
+					<Text>
+                    Get more with premium access!
+					</Text>
+				</View>
+			</ScrollView>
+		</View>
+	);
+};
+
+const getStyles = (appLayout: Object): Object => {
+	const { height, width } = appLayout;
+	const isPortrait = height > width;
+	const deviceWidth = isPortrait ? width : height;
+	const padding = deviceWidth * Theme.Core.paddingFactor;
+
+	return {
+		container: {
+			flex: 1,
+			backgroundColor: Theme.Core.appBackground,
+		},
+		body: {
+			flex: 0,
+			alignItems: 'center',
+			justifyContent: 'center',
+			backgroundColor: '#fff',
+			...Theme.Core.shadow,
+			marginHorizontal: padding,
+			marginVertical: padding * 2,
+			paddingBottom: padding * 2,
+		},
+	};
+};
+
+export default PremiumUpgradeScreen;
