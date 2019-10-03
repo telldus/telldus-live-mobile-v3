736e599cd1ededf259c090363a525e19adeb9cb1
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2019-10-03 15:26:43 +0530

    Add component ModeIconBlock, LabelBlock

---
+++ js/App/Components/ThermostatControl/SubViews/LabelBlock.js
@@ -0,0 +1,57 @@
+/**
+ * Copyright 2016-present Telldus Technologies AB.
+ *
+ * This file is part of the Telldus Live! app.
+ *
+ * Telldus Live! app is free : you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Telldus Live! app is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Telldus Live! app.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// @flow
+
+'use strict';
+
+import React from 'react';
+import {
+	TouchableOpacity,
+} from 'react-native';
+
+import {
+	Text,
+} from '../../../../BaseComponents';
+
+const LabelBlock = (props: Object): Object => {
+	const {
+		onPressRow,
+		textStyle,
+		label,
+	} = props;
+	function onPress() {
+		onPressRow(1);
+	}
+	return (
+		<TouchableOpacity style={{
+			flex: 1,
+			flexDirection: 'row',
+			alignItems: 'center',
+			justifyContent: 'flex-start',
+		}} onPress={onPress}>
+			<Text style={textStyle}>
+				{label}
+			</Text>
+		</TouchableOpacity>
+	);
+};
+
+export default LabelBlock;
+
---
+++ js/App/Components/ThermostatControl/SubViews/ModeIconBlock.js
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2016-present Telldus Technologies AB.
+ *
+ * This file is part of the Telldus Live! app.
+ *
+ * Telldus Live! app is free : you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Telldus Live! app is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Telldus Live! app.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// @flow
+
+'use strict';
+
+import React from 'react';
+import {
+	TouchableOpacity,
+} from 'react-native';
+
+const ModeIconBlock = (props: Object): Object => {
+	const {
+		onPressRow,
+		style,
+		children,
+	} = props;
+	function onPress() {
+		onPressRow(1);
+	}
+	return (
+		<TouchableOpacity style={style} onPress={onPress}>
+			{children}
+		</TouchableOpacity>
+	);
+};
+
+export default ModeIconBlock;
+
