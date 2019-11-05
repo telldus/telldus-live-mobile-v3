eec279e9d0af0f89b2da882a8496fea8ac3b90b6
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2019-11-05 10:24:17 +0530

    Use LaunchScreen.xib instead of storyboard

--- ios/TelldusLiveApp/AppDelegate.m
+++ ios/TelldusLiveApp/AppDelegate.m
@@ -47,7 +47,7 @@
 	rootViewController.view = rootView;
 	self.window.rootViewController = rootViewController;
 	[self.window makeKeyAndVisible];
-	[RNSplashScreen show];
+    [RNSplashScreen showSplash:@"LaunchScreen" inRootView:rootView];;
 	return YES;
 }
 
