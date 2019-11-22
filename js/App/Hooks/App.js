045ed5024697babe0014bf1bd4622c050d16b54f
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2019-11-22 15:11:12 +0530

    Add new[First ever] hook 'useRelativeIntl'
    See #895.

---
+++ js/App/Hooks/App.js
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2016-present Telldus Technologies AB.
+ *
+ * This file is part of the Telldus Live! app.
+ *
+ * Telldus Live! app is free : you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Telldus Live! app is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Telldus Live! app.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+// @flow
+'use strict';
+import {
+	createIntl,
+	createIntlCache,
+} from 'react-intl';
+import { useSelector } from 'react-redux';
+import DeviceInfo from 'react-native-device-info';
+
+const useRelativeIntl = (gatewayTimezone?: string): Object => {
+
+	const { defaultSettings = {} } = useSelector((state: Object): Object => state.app);
+	let { language = {} } = defaultSettings;
+	let locale = language.code;
+
+	gatewayTimezone = gatewayTimezone || DeviceInfo.getTimezone();
+
+	const cache = createIntlCache();
+	return createIntl({
+		locale,
+		timeZone: gatewayTimezone,
+	}, cache);
+};
+
+module.exports = {
+	useRelativeIntl,
+};
