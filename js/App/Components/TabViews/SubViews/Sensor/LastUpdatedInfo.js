0523eb0a01f005e49b895b5a368021c3f2b93329
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2019-11-22 18:27:02 +0530

    Add new sensor subcomponent 'LastUpdatedInfo'
    See #895.

---
+++ js/App/Components/TabViews/SubViews/Sensor/LastUpdatedInfo.js
@@ -0,0 +1,65 @@
+/**
+ * Copyright 2016-present Telldus Technologies AB.
+ *
+ * This file is part of the Telldus Live! app.
+ *
+ * Telldus Live! app is free : you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Telldus Live! app is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Telldus Live! app.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// @flow
+
+'use strict';
+
+import React from 'react';
+import { RawIntlProvider } from 'react-intl';
+
+import {
+	FormattedRelative,
+} from '../../../../../BaseComponents';
+
+import {
+	formatSensorLastUpdate,
+} from '../../../../Lib/SensorUtils';
+
+import {
+	useRelativeIntl,
+} from '../../../../Hooks/App';
+
+const LastUpdatedInfo = (props: Object): Object => {
+	const {
+		textStyle,
+		gatewayTimezone,
+		value,
+		numeric,
+		updateIntervalInSeconds,
+	} = props;
+
+	const intl = useRelativeIntl(gatewayTimezone);
+	function formatSensorLastUpdateFunc(time: string): string {
+		return formatSensorLastUpdate(time, intl);
+	}
+
+	return (
+		<RawIntlProvider value={intl}>
+			<FormattedRelative
+				value={value}
+				numeric={numeric}
+				updateIntervalInSeconds={updateIntervalInSeconds}
+				formatterFunction={formatSensorLastUpdateFunc}
+				textStyle={textStyle}/>
+		</RawIntlProvider>
+	);
+};
+
+export default LastUpdatedInfo;
