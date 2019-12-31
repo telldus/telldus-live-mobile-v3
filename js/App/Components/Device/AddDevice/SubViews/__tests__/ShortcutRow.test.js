5c622cb815938f4ed42e4769e4845a6b1a2a1a57
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2019-12-31 11:38:11 +0530

    Add unit test for component 'ShortcutRow'

---
+++ js/App/Components/Device/AddDevice/SubViews/__tests__/ShortcutRow.test.js
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2016-present Telldus Technologies AB.
+ *
+ * This file is part of the Telldus Live! app.
+ *
+ * Telldus Live! app is free : you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Telldus Live! app is distributed in the hope this it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Telldus Live! app.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+import React from 'react';
+import {
+	Dimensions,
+} from 'react-native';
+import renderer from 'react-test-renderer';
+import * as redux from 'react-redux';
+
+import ShortcutRow from '../ShortcutRow';
+
+let {height, width} = Dimensions.get('window');
+
+const spy = jest.spyOn(redux, 'useSelector');
+spy.mockReturnValue(
+	{
+		layout: {
+			height,
+			width,
+		},
+	}
+);
+
+it('renders ShortcutRow using Snapshots', () => {
+	expect(renderer.create(
+		<ShortcutRow/>
+	)).toMatchSnapshot();
+});
