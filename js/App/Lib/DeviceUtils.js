27b6b1300902546102308c7b3dbf053859718bff
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2019-12-31 14:38:07 +0530

    Add Device Util method 'prepare433ModelName'

--- js/App/Lib/DeviceUtils.js
+++ js/App/Lib/DeviceUtils.js
@@ -86,6 +86,16 @@ function openURL(url: string): any {
 	  });
 }
 
+const prepare433ModelName = (locale: string, lang: Array<Object> = [], modelNameDef: string): string => {
+	let name = modelNameDef;
+	for (let i = 0; i < lang.length; i++) {
+		if (lang[i].lang === locale) {
+			name = lang[i].modelName;
+		}
+	}
+	return name;
+};
+
 module.exports = {
 	...deviceUtils,
 	...addDeviceUtils,
@@ -94,4 +104,5 @@ module.exports = {
 	prepareNo433MHzSupportDialogueData,
 	openURL,
 	goToWebShop,
+	prepare433ModelName,
 };
