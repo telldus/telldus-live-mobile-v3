01861ed83bb682fe35e8b5be1d84bc68c0180d7d
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2020-01-16 16:57:06 +0530

    Update screen 'AddEditGeoFence'
    See #43.

--- js/App/Components/GeoFence/AddEditGeoFence.js
+++ js/App/Components/GeoFence/AddEditGeoFence.js
@@ -23,10 +23,21 @@
 
 import React from 'react';
 import {
+	StyleSheet,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {
 	View,
-	Text,
 	FloatingButton,
+	Text,
 } from '../../../BaseComponents';
+import MapView, {
+	AnimatedRegion,
+	Circle,
+	Callout,
+} from 'react-native-maps';
+import Theme from '../../Theme';
 
 type Props = {
     navigation: Object,
@@ -49,29 +60,109 @@ const AddEditGeoFence = (props: Props): Object => {
 
 	const {
 		container,
+		mapStyle,
+		calloutStyle,
+		calloutInnerView,
+		editIconSize,
+		calloutText,
 	} = getStyles(appLayout);
+
+	const region = new AnimatedRegion({
+		latitude: 55.70584,
+		longitude: 13.19321,
+		latitudeDelta: 0.24442,
+		longitudeDelta: 0.24442,
+	});
+
+	const coordinate = {
+		latitude: 55.70584,
+		longitude: 13.19321,
+	};
+
+	const circleCenter = {
+		latitude: 55.70584,
+		longitude: 13.19321,
+	};
+
+	// eslint-disable-next-line no-unused-vars
+	let marker;
+	function setRefMarker(ref: any) {
+		marker = ref;
+	}
+
+	// eslint-disable-next-line no-unused-vars
+	let map;
+	function setRefMap(ref: any) {
+		map = ref;
+	}
+
+	function onPressCallout() {
+		navigation.navigate({
+			routeName: 'EditGeoFence',
+			key: 'EditGeoFence',
+		});
+	}
 
 	return (
 		<View style={container}>
-			<Text>
-            AddEditGeoFence
-			</Text>
+			<MapView.Animated
+				style={mapStyle}
+				ref={setRefMap}
+				region={region}>
+				<MapView.Marker.Animated
+					ref={setRefMarker}
+					coordinate={coordinate}>
+					<Callout style={calloutStyle} onPress={onPressCallout}>
+						<View style={calloutInnerView}>
+							<Text style={calloutText}>Area name</Text>
+							<Icon name={'edit'} size={editIconSize} color={Theme.Core.rowTextColor}/>
+						</View>
+					</Callout>
+				</MapView.Marker.Animated>
+				<Circle
+					center={circleCenter}
+					radius={3000}
+					fillColor={'#e2690150'}
+					strokeColor={Theme.Core.brandSecondary}/>
+			</MapView.Animated>
 			<FloatingButton
 				onPress={onPressNext}
-				imageSource={{uri: 'right_arrow_key'}}
-			/>
+				imageSource={{uri: 'icon_plus'}}/>
 		</View>
 	);
 };
 
 const getStyles = (appLayout: Object): Object => {
-	// const { height, width } = appLayout;
-	// const isPortrait = height > width;
-	// const deviceWidth = isPortrait ? width : height;
+	const { height, width } = appLayout;
+	const isPortrait = height > width;
+	const deviceWidth = isPortrait ? width : height;
+
+	const {
+		brandSecondary,
+	} = Theme.Core;
+
+	const editIconSize = deviceWidth * 0.04;
 
 	return {
 		container: {
 			flex: 1,
+		},
+		mapStyle: {
+			...StyleSheet.absoluteFillObject,
+		},
+		calloutStyle: {
+			padding: 5,
+		},
+		calloutInnerView: {
+			flexDirection: 'row',
+			alignItems: 'center',
+			justifyContent: 'center',
+		},
+		editIconSize,
+		calloutText: {
+			fontSize: editIconSize,
+			color: brandSecondary,
+			marginRight: 10,
 		},
 	};
 };
