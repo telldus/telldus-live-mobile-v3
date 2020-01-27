b98e232ede2a604bdc6b4860f75a469c2bd6f528
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2020-01-27 15:21:52 +0530

    Do not render svg circular slider with invalid angle
    This commit prevents crash while redering circular
    slider with wrong attributes, say calculated 'angleLength'.
    In that case default value is used.
    
    Changelog: Fix crash in thermostat control screen.

--- js/App/Components/ThermostatControl/HeatControlWheelModes.js
+++ js/App/Components/ThermostatControl/HeatControlWheelModes.js
@@ -493,10 +493,12 @@ render(): Object | null {
 
 	let startAngleF = this.initialAngle, angleLengthF = 100;
 	const hasValidMinMax = typeof minVal === 'number' && typeof maxVal === 'number';
-	if (hasValidMinMax) {
+	if (hasValidMinMax && !isNaN(angleLength) && !isNaN(startAngle)) {
 		startAngleF = startAngle;
 		angleLengthF = angleLength;
 	}
+
+	const showCircularSlider = typeof startAngleF === 'number' && typeof angleLengthF === 'number';
 
 	const showControlIcons = controllingMode !== 'off' && controllingMode !== 'fan';
 
@@ -512,7 +514,7 @@ render(): Object | null {
 						size={iconSize}/>
 				</TouchableOpacity>
 				}
-				<CircularSlider
+				{showCircularSlider && <CircularSlider
 					SVGKey={SVGKey}
 					coverStyle={coverStyle}
 					startAngle={startAngleF}
@@ -537,6 +539,7 @@ render(): Object | null {
 					onReleaseStopKnob={hasValidMinMax ? this.onEndSlide : null}
 					onPressSliderPath={hasValidMinMax ? this.onPressSliderPath : null}
 				/>
+				}
 				<ControlInfoBlock
 					appLayout={appLayout}
 					baseColor={baseColor}
