38dc81acbbc415f1a77bd5504943ec5f532e0f24
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2020-02-04 08:51:16 +0530

    Add push related method 'checkPlayServices' - android

--- js/App/Components/Push.android.js
+++ js/App/Components/Push.android.js
@@ -160,6 +160,56 @@ const Push = {
 	navigateToPurchasePremium: () => {
 		navigate('PremiumUpgradeScreen', {}, 'PremiumUpgradeScreen');
 	},
+	checkPlayServices(): Object {
+		const utils = firebase.utils();
+
+		const {
+		  status,
+		  isAvailable,
+		  hasResolution,
+		  isUserResolvableError,
+		} = utils.playServicesAvailability;
+
+		// all good and valid \o/
+		if (isAvailable) {
+			return utils.playServicesAvailability;
+		}
+
+		// if the user can resolve the issue i.e by updating play services
+		// then call Google Play's own/default prompting functionality
+		if (isUserResolvableError || hasResolution) {
+		  switch (status) {
+				case 1:
+			  // SERVICE_MISSING - Google Play services is missing on this device.
+			  // show something to user
+			  // and then attempt to install if necessary
+					utils.makePlayServicesAvailable();
+					return utils.playServicesAvailability;
+				case 2:
+			  // SERVICE_VERSION_UPDATE_REQUIRED - The installed version of Google Play services is out of date.
+			  // show something to user
+			  // and then attempt to update if necessary
+			   utils.resolutionForPlayServices();
+			   return utils.playServicesAvailability;
+					// TODO handle other cases as necessary, see link below for all codes and descriptions
+					// TODO e.g. https://developers.google.com/android/reference/com/google/android/gms/common/ConnectionResult#SERVICE_VERSION_UPDATE_REQUIRED
+				default:
+			  // some default dialog / component?
+			  if (isUserResolvableError) {
+				   utils.promptForPlayServices();
+				  return utils.playServicesAvailability;
+			  }
+			  if (hasResolution) {
+						utils.resolutionForPlayServices();
+				  return utils.playServicesAvailability;
+			  }
+		  }
+		}
+
+		// There's no way to resolve play services on this device
+		// probably best to show a dialog / force crash the app
+		return utils.playServicesAvailability;
+	  },
 };
 
 module.exports = Push;
