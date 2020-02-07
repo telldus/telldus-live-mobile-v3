9458d8caceec14fe2df56eccd840c14dbcab4798
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2020-02-07 19:36:52 +0530

    Update UI when add/edit thermostat schedule

--- js/App/Components/Schedule/ActionThermostat.js
+++ js/App/Components/Schedule/ActionThermostat.js
@@ -300,9 +300,7 @@ export default class ActionThermostat extends View<null, Props, State> {
 						hideTemperatureControl={hideTemperatureControl}
 						intl={intl}
 						source="Schedule_ActionThermostat"
-						timeoutPlusMinus={0}
-						gradientColorFromOverride={'#004D92'}
-						gradientColorToOverride={'#e26901'}/>
+						timeoutPlusMinus={0}/>
 				</ScrollView>
 				{(this.supportedModes && this.supportedModes.length > 0) ? <FloatingButton
 					onPress={this.selectAction}
