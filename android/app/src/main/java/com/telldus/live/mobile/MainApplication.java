9e16e80400d290b2c06c0ea0e409d930fc591bed
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2020-02-11 20:06:15 +0530

    Add support for Google analytics

--- android/app/src/main/java/com/telldus/live/mobile/MainApplication.java
+++ android/app/src/main/java/com/telldus/live/mobile/MainApplication.java
@@ -38,6 +38,7 @@ import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 import io.invertase.firebase.fabric.crashlytics.RNFirebaseCrashlyticsPackage;
 import io.invertase.firebase.config.RNFirebaseRemoteConfigPackage;
+import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
 import com.telldus.live.mobile.WidgetPackage;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -59,6 +60,7 @@ public class MainApplication extends Application implements ReactApplication {
 			packages.add(new RNFirebaseNotificationsPackage());
 			packages.add(new RNFirebaseCrashlyticsPackage());
 			packages.add(new RNFirebaseRemoteConfigPackage());
+			packages.add(new RNFirebaseAnalyticsPackage());
 			packages.add(new WidgetPackage());
 			return packages;
 		}
