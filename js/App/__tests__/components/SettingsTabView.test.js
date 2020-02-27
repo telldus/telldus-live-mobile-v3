397ff21d36caf3c10107740e7dfe66627f54fbcd
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2020-02-27 20:20:40 +0530

    Remove actions test from component
    ADD_TO_DASHBOARD
    REMOVE_FROM_DASHBOARD

--- js/App/__tests__/components/SettingsTabView.test.js
+++ js/App/__tests__/components/SettingsTabView.test.js
@@ -4,7 +4,6 @@ import { IntlProvider } from 'react-intl';
 import { shallow, mockStore } from '../../../Utils/TelldusTestSuite';
 
 import SettingsTab from '../../Components/Device/DeviceDetails/SettingsTab';
-import { addToDashboard, removeFromDashboard } from '../../Actions';
 import { LearnButton } from '../../Components/TabViews/SubViews';
 
 
@@ -13,6 +12,7 @@ describe('<SettingsTab />', () => {
 	let wrapper, store;
 	beforeEach(()=>{
 		store = mockStore({});
+
 		wrapper = shallow(
 			<Provider store={store}>
 				<IntlProvider>
@@ -28,29 +28,6 @@ describe('<SettingsTab />', () => {
 		const screen = wrapper.props().children.props.children.type.displayName;
 		expect(screen).toEqual('Connect(SettingsTab)');
 	});
-
-	it(' check addToDashboard action on dispatching ', () => {
-		const data = [ {
-			id: 333,
-			kind: 'addToDashboard',
-			type: 'ADD_TO_DASHBOARD',
-		}];
-		store.dispatch(addToDashboard('addToDashboard', 333));
-		let action = store.getActions();
-		expect(action).toEqual(data);
-	});
-
-	it(' check removeFromDashboard action on dispatching ', () => {
-		const data = [{
-			id: 777,
-			kind: 'removeFromDashboard',
-			type: 'REMOVE_FROM_DASHBOARD',
-		}];
-		store.dispatch(removeFromDashboard('removeFromDashboard', 777));
-		let action = store.getActions();
-		expect(action).toEqual(data);
-	});
-
 });
 
 describe('LearnButton', () => {
