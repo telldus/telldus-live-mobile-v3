c81815a44709b645fa663d3ed927a635330015a8
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2020-02-27 20:40:54 +0530

    Update unit tests - actions
    - addToDashboard and removeFromDashboard

--- js/App/__tests__/actions/DashboardAction.test.js
+++ js/App/__tests__/actions/DashboardAction.test.js
@@ -1,29 +1,57 @@
 import * as actions from '../../Actions/Dashboard.js';
-
+import { configureStore } from '../../Store/ConfigureStore';
 
 describe('(Dashboard) removeFromDashboard ', () => {
-	it('should return  REMOVE_FROM_DASHBOARD', () => {
-		const Kind = 'device';
-		const Id = '10';
-		const expectedAction = {
-			type: 'REMOVE_FROM_DASHBOARD',
-			kind: Kind,
-			id: Id,
-		};
-		expect(actions.removeFromDashboard(Kind, Id)).toEqual(expectedAction);
+	let store;
+	const dashboardId = 'dashboardId';
+	const userId = '1';
+
+	beforeAll(() => {
+		store = configureStore();
+		store.dispatch({type: 'SELECT_DASHBOARD', payload: {
+			dashboardId,
+		}});
+		store.dispatch({type: 'RECEIVED_USER_PROFILE', payload: {
+			email: userId,
+		}});
+	});
+
+	it('should not contain the removed id', () => {
+		const kind = 'device';
+		const id = '10';
+
+		store.dispatch(actions.removeFromDashboard(kind, id));
+
+		const deviceIdsCurrent = store.getState().dashboard.deviceIds[userId][dashboardId];
+
+		expect(deviceIdsCurrent).not.toContain(id);
 	});
 });
 
 
 describe('(Dashboard) addToDashboard ', () => {
-	it('should return  ADD_TO_DASHBOARD', () => {
-		const Kind = 'device';
-		const Id = '10';
-		const expectedAction = {
-			type: 'ADD_TO_DASHBOARD',
-			kind: Kind,
-			id: Id,
-		};
-		expect(actions.addToDashboard(Kind, Id)).toEqual(expectedAction);
+	let store;
+	const dashboardId = 'dashboardId';
+	const userId = '1';
+
+	beforeAll(() => {
+		store = configureStore();
+		store.dispatch({type: 'SELECT_DASHBOARD', payload: {
+			dashboardId,
+		}});
+		store.dispatch({type: 'RECEIVED_USER_PROFILE', payload: {
+			email: userId,
+		}});
+	});
+
+	it('should not contain the removed id', () => {
+		const kind = 'device';
+		const id = '10';
+
+		store.dispatch(actions.addToDashboard(kind, id));
+
+		const deviceIdsCurrent = store.getState().dashboard.deviceIds[userId][dashboardId];
+
+		expect(deviceIdsCurrent).toContain(id);
 	});
 });
