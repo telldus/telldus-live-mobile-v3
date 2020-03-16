6609f2ee1f86e69786a92cced38538839e26b01a
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2020-03-16 09:57:03 +0530

    Add reducer 'navigation' to redux persist blacklist

--- js/App/Reducers/index.js
+++ js/App/Reducers/index.js
@@ -39,7 +39,7 @@ import { reducers } from 'live-shared-data';
 const config = {
 	key: localStorageKey,
 	storage: AsyncStorage,
-	blacklist: ['dimmer', 'schedule', 'liveApi', 'modal', 'addDevice'],
+	blacklist: ['dimmer', 'schedule', 'liveApi', 'modal', 'addDevice', 'navigation'],
 	migrate: migrations,
 };
 
