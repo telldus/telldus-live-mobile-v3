fe90a2fa56f9fb07921fd499fbe82dbb1a957ce7
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2020-03-23 13:09:19 +0530

    Update handling of available purchases on app start - iOS

--- js/App/Components/PostLoginNavigatorCommon.js
+++ js/App/Components/PostLoginNavigatorCommon.js
@@ -61,6 +61,7 @@ import {
 	setGatewayRelatedGAProperties,
 	onReceivedInAppPurchaseProducts,
 	onReceivedInAppAvailablePurchases,
+	reportIapAtServer,
 } from '../Actions';
 import { getUserProfile as getUserProfileSelector } from '../Reducers/User';
 import { hideDimmerStep } from '../Actions/Dimmer';
@@ -195,8 +196,26 @@ async componentDidMount() {
 			const products = await RNIap.getSubscriptions(subs);
 			dispatch(onReceivedInAppPurchaseProducts(products));
 
-			const purchases = await RNIap.getAvailablePurchases();// Those not called 'finishTransaction' post-purchase
+			const purchases = await RNIap.getAvailablePurchases() || [];// Those not called 'finishTransaction' post-purchase
 			dispatch(onReceivedInAppAvailablePurchases(purchases));
+			purchases.forEach((purchase: Object) => {
+				const idsAutoRenewing = ['premiumauto'];// TODO update with the actual auto renewing susbscriptions
+				const {
+					productId,
+				} = purchase;
+
+				// Fix for ISSUE 1 reported below. But with a cost one unnecessary
+				// API call/report(auto renewing subscription).
+				const isAutoRenewing = idsAutoRenewing.indexOf(productId) === -1;
+				if (!isAutoRenewing) {
+					dispatch(reportIapAtServer(purchase));
+				}
+				// TODO: If auto renewing subscription:
+				// 1- Take the one with latest 'originTransactionDateIOS'
+				// 2- Report that alone at the server
+				// Even after reporting and calling 'finishTransaction' and 'finishTransactionIOS',
+				// Unlike non-renew, it will still be present in this list.
+			});
 			// TODO: These are the purchases made successfully but failed to
 			// report at our server(createTransaction not success), may be try to report now?
 
