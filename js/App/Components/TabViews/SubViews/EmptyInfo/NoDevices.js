5a6f6820fba8968eacc9baf31c35baa653f9f019
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2020-03-26 16:55:46 +0530

    Add new component
    NoGateways
    EmptyTabTemplate
    NoDevices
    NoSensors
    
    See #1003 and See #1005

---
+++ js/App/Components/TabViews/SubViews/EmptyInfo/NoDevices.js
@@ -0,0 +1,73 @@
+/**
+ * Copyright 2016-present Telldus Technologies AB.
+ *
+ * This file is part of the Telldus Live! app.
+ *
+ * Telldus Live! app is free : you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Telldus Live! app is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Telldus Live! app.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// @flow
+
+'use strict';
+
+import React, {
+	memo,
+	useCallback,
+} from 'react';
+import { useIntl } from 'react-intl';
+import { useSelector } from 'react-redux';
+
+import EmptyTabTemplate from './EmptyTabTemplate';
+
+import i18n from '../../../../Translations/common';
+
+import {
+	navigate,
+} from '../../../../Lib/NavigationService';
+
+type Props = {
+};
+
+const NoDevices = (props: Props): Object => {
+
+	const { byId } = useSelector((state: Object): Object => state.gateways);
+
+	const intl = useIntl();
+	const {
+		formatMessage,
+	} = intl;
+
+	const onPress = useCallback(() => {
+		const gatewaysLen = Object.keys(byId).length;
+		const singleGateway = gatewaysLen === 1;
+		navigate('AddDevice', {
+			selectLocation: !singleGateway,
+			gateway: singleGateway ? {
+				...byId[Object.keys(byId)[0]],
+			} : null,
+		}, 'AddDevice');
+	}, [byId]);
+
+	return (
+		<EmptyTabTemplate
+			onPress={onPress}
+			bottonLabel={formatMessage(i18n.iconAddPhraseOneD)}
+			headerText={formatMessage(i18n.messageNoDeviceTitle)}
+			bodyText={formatMessage(i18n.messageNoDeviceContentAddZ)}
+			isLoading={false}
+			icon={'device-alt'}/>
+	);
+};
+
+export default memo<Object>(NoDevices);
---
+++ js/App/Components/TabViews/SubViews/EmptyInfo/NoGateways.js
@@ -0,0 +1,92 @@
+/**
+ * Copyright 2016-present Telldus Technologies AB.
+ *
+ * This file is part of the Telldus Live! app.
+ *
+ * Telldus Live! app is free : you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Telldus Live! app is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Telldus Live! app.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// @flow
+
+'use strict';
+
+import React, {
+	memo,
+	useCallback,
+	useState,
+} from 'react';
+import { useIntl } from 'react-intl';
+import { useDispatch } from 'react-redux';
+
+import EmptyTabTemplate from './EmptyTabTemplate';
+
+import {
+	addNewGateway,
+} from '../../../../Actions/Gateways';
+import {
+	showToast,
+} from '../../../../Actions/App';
+
+import i18n from '../../../../Translations/common';
+
+import {
+	navigate,
+} from '../../../../Lib/NavigationService';
+
+type Props = {
+};
+
+const NoGateways = (props: Props): Object => {
+
+	const [ isLoading, setIsLoading ] = useState(false);
+
+	const dispatch = useDispatch();
+
+	const intl = useIntl();
+	const {
+		formatMessage,
+	} = intl;
+
+	const onPress = useCallback(() => {
+		setIsLoading(true);
+		dispatch(addNewGateway())
+			.then((response: Object) => {
+				if (response.client) {
+					setIsLoading(false);
+					navigate(
+						'AddLocation',
+						{ clients: response.client },
+						'AddLocation');
+				} else {
+					throw response;
+				}
+			}).catch((err: any) => {
+				setIsLoading(false);
+				dispatch(showToast(err.message || formatMessage(i18n.unknownError)));
+			});
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	return (
+		<EmptyTabTemplate
+			onPress={isLoading ? undefined : onPress}
+			bottonLabel={formatMessage(i18n.gatewayAdd)}
+			headerText={formatMessage(i18n.messageNoGatewayTitle)}
+			bodyText={formatMessage(i18n.messageNoGatewayContent)}
+			isLoading={isLoading}
+			icon={'location'}/>
+	);
+};
+
+export default memo<Object>(NoGateways);
