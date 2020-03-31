16c8b58e43894ec259b123ff1943634f0ee03c5a
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2020-03-31 12:21:57 +0530

    Update 'buildGitlabRunner' - Android
    Write 'DEPLOY_STORE' to config.local.js

--- android/buildGitlabRunner.sh
+++ android/buildGitlabRunner.sh
@@ -22,7 +22,8 @@ module.exports = {
     googleMapsAPIKey: '${GOOGLE_MAPS_API_KEY}',
     webClientId: '${GOOGLE_WEB_CLIENT_ID}',
     osTicketKey: '${OSTICKET_KEY}',
-    twitterAuth: ${TWITTER_AUTH}
+    twitterAuth: ${TWITTER_AUTH},
+    deployStore: ${DEPLOY_STORE}
 };
 EOF
 
