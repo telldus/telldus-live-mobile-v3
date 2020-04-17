24577300a8e0266fe2c7ba86f1d693cb66718dbe
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2020-04-17 09:34:15 +0530

    Memoise function - ViewPremiumBenefitsButton
    See #999.

--- js/App/Components/Premium/SubViews/ViewPremiumBenefitsButton.js
+++ js/App/Components/Premium/SubViews/ViewPremiumBenefitsButton.js
@@ -22,7 +22,9 @@
 
 'use strict';
 
-import React from 'react';
+import React, {
+	useCallback,
+} from 'react';
 import { useSelector } from 'react-redux';
 import { useIntl } from 'react-intl';
 
@@ -54,9 +56,11 @@ const ViewPremiumBenefitsButton = (props: Object): Object => {
 		buttonStyle,
 		textStyle,
 	} = getStyles(layout);
-	function onPress() {
+
+	const onPress = useCallback(() => {
 		navigation.navigate('PremiumBenefitsScreen');
-	}
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
 	return (
 		<>
