4e0f0ab5d961f0875997698c241088e835764969
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2020-06-02 10:37:42 +0530

    Add support for app theme - Throbber
    See #231.

--- js/BaseComponents/Throbber.js
+++ js/BaseComponents/Throbber.js
@@ -25,13 +25,13 @@ import {Animated, StyleSheet} from 'react-native';
 
 import View from './View';
 import IconTelldus from './IconTelldus';
-import Theme from '../App/Theme';
 const AnimatedIconTelldus = Animated.createAnimatedComponent(IconTelldus);
 
 type Props = {
 	throbberContainerStyle?: Array<any> | Object,
 	throbberStyle?: Array<any> | Object,
 	throbSpeed?: number,
+	level?: number,
 };
 
 export default class Throbber extends Component<Props, null> {
@@ -76,6 +76,7 @@ export default class Throbber extends Component<Props, null> {
 		let {
 			throbberContainerStyle,
 			throbberStyle,
+			level,
 		} = this.props;
 
 		let interpolatedValue = this.animatedValue.interpolate({
@@ -85,12 +86,14 @@ export default class Throbber extends Component<Props, null> {
 
 		return (
 			<View style={[styles.throbberContainer, throbberContainerStyle]}>
-				<AnimatedIconTelldus icon="loading" style={[styles.throbber, throbberStyle, {
-					transform: [{
-						rotate: interpolatedValue,
-					}],
-				},
-				]}/>
+				<AnimatedIconTelldus
+					level={level}
+					icon="loading" style={[styles.throbber, throbberStyle, {
+						transform: [{
+							rotate: interpolatedValue,
+						}],
+					},
+					]}/>
 			</View>
 		);
 	}
@@ -103,6 +106,5 @@ const styles = StyleSheet.create({
 	},
 	throbber: {
 		fontSize: 25,
-		color: Theme.Core.brandSecondary,
 	},
 });
