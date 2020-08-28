ac6e2f0fed5c4f0c52b8afc7ab9f5bda53b67e24
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2020-08-28 19:41:52 +0530

    Initial commit - add target widget(iOS)
    This commit is initial work to support
    homescreen widget in iOS(14).
    
    See #1036.

---
+++ ios/HomescreenWidget/HomescreenWidget.swift
@@ -0,0 +1,71 @@
+//
+//  HomescreenWidget.swift
+//  HomescreenWidget
+//
+//  Created by Rimnesh Fernandez on 28/08/20.
+//  Copyright © 2020 Telldus Technologies AB. All rights reserved.
+//
+
+import WidgetKit
+import SwiftUI
+import Intents
+
+struct Provider: IntentTimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
+    }
+
+    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), configuration: configuration)
+        completion(entry)
+    }
+
+    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, configuration: configuration)
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let configuration: ConfigurationIntent
+}
+
+struct HomescreenWidgetEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        Text(entry.date, style: .time)
+        Text("Telldus Live! Mobile")
+    }
+}
+
+@main
+struct HomescreenWidget: Widget {
+    let kind: String = "HomescreenWidget"
+
+    var body: some WidgetConfiguration {
+        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
+            HomescreenWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("Telldus Widget")
+        .description("This is an example widget.")
+    }
+}
+
+struct HomescreenWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        HomescreenWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
