75e98522ddca8039433e09abee76520398d5f977
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2020-09-16 18:42:52 +0530

    Add new base components - SubHeaderText, HeaderText
    See #1031.

---
+++ js/BaseComponents/SubHeaderText.js
@@ -0,0 +1,76 @@
+/**
+ * Copyright 2016-present Telldus Technologies AB.
+ *
+ * This file is part of the Telldus Live! app.
+ *
+ * Telldus Live! app is free : you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Telldus Live! app is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Telldus Live! app.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// @flow
+
+'use strict';
+
+import React, {
+	memo,
+} from 'react';
+import { useSelector } from 'react-redux';
+
+import Text from './Text';
+
+type Props = {
+	textStyle?: Object,
+	text: string,
+};
+
+const SubHeaderText = memo<Object>((props: Props): Object => {
+	const {
+		text,
+		textStyle,
+	} = props;
+
+	const { layout } = useSelector((state: Object): Object => state.app);
+
+	const {
+		textStyleDef,
+	} = getStyles({
+		layout,
+	});
+
+	return (
+		<Text
+			level={4}
+			style={[
+				textStyleDef,
+				textStyle,
+			]}>
+			{text}
+		</Text>
+	);
+});
+
+const getStyles = ({
+	layout,
+}: Object): Object => {
+	const { height, width } = layout;
+	const isPortrait = height > width;
+	const deviceWidth = isPortrait ? width : height;
+
+	return {
+		textStyleDef: {
+			fontSize: Math.floor(deviceWidth * 0.05),
+		},
+	};
+};
+
+export default SubHeaderText;
