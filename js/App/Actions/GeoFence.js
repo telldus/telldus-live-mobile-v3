ca3397ccbeb9c06b4bf2deb7bfc832c41f96f886
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2020-10-12 15:16:39 +0530

    Do not execute fence action if the related account is basic
    Adding premium or not check before executing
    fence actions, to handle likely scenario where
    user might have added fences when in premium
    and and has expired.

--- js/App/Actions/GeoFence.js
+++ js/App/Actions/GeoFence.js
@@ -359,13 +359,18 @@ function handleActions(actions: Object, userId: string, eventUUID: string, extra
 		}
 
 		const { user: { accounts = {} } } = getState();
-		const { accessToken } = accounts[userId];
+		const {
+			accessToken,
+			pro,
+		} = accounts[userId] || {};
 		dispatch(debugGFSetCheckpoint({
 			checkpoint: 'handleActions-1',
 			eventUUID,
 			accessToken,
 		}));
-		if (!accessToken) {
+
+		const isBasic = isBasicUser(pro);
+		if (!accessToken || isBasic) {
 			return Promise.resolve('done');
 		}
 
