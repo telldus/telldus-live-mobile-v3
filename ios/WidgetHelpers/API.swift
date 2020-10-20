865ade5687380b273d2d172adff2707a480d4cea
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2020-10-20 11:55:31 +0530

    Update class 'API' - iOS
    For API error invalid/expired token, automatically
    tries to refresh the token, if succes updates the
    new token in the keychain, and continues the flow.
    
    See #1036.

--- ios/WidgetHelpers/API.swift
+++ ios/WidgetHelpers/API.swift
@@ -9,97 +9,96 @@
 import Foundation
 
 enum AppError: Error {
-    case networkError(Error)
-    case dataNotFound
-    case jsonParsingError(Error)
-    case invalidStatusCode(Int)
-    case invalidToken
-    case unknownError(String)
-    case noAuthToken
+  case networkError(Error)
+  case dataNotFound
+  case jsonParsingError(Error)
+  case invalidStatusCode(Int)
+  case invalidToken
+  case unknownError(String)
+  case noAuthToken
 }
 
 //Result enum to show success or failure
 enum Result<T> {
-    case success(T)
-    case failure(AppError)
+  case success(T)
+  case failure(AppError)
 }
 
 class API {
   func callEndPoint(_ params: String, completion: @escaping (Result<Any>) -> Void) {
-    
     let data = WidgetModule().getSecureData()
     guard data != nil else {
       completion(Result.failure(AppError.noAuthToken))
-        return
+      return
     }
+
     let dataDict = Utilities().stringToDictionary(string: data!)
-    print("TEST dataDict \(dataDict)")
     guard dataDict != nil else {
       completion(Result.failure(AppError.noAuthToken))
-        return
+      return
     }
     
     let accessToken = dataDict!["accessToken"] as! String?
-    print("TEST accessToken \(accessToken)")
     guard accessToken != nil else {
       completion(Result.failure(AppError.noAuthToken))
-        return
+      return
     }
-  
+    
     let url = URL(string: "\(Constants.telldusAPIServer)/oauth2\(params)")!
     var request = URLRequest(url: url, timeoutInterval: 30);
     request.addValue("application/json", forHTTPHeaderField: "Content-Type")
     request.addValue("application/json", forHTTPHeaderField: "Accept")
     request.addValue("Bearer \(accessToken!)", forHTTPHeaderField: "Authorization")
-    print("TEST url \(url)")
     let task = URLSession.shared.dataTask(with: request) {(data, response, error) in
-        guard error == nil else {
-            completion(Result.failure(AppError.networkError(error!)))
-            return
-        }
-
-        guard let data = data else {
-            completion(Result.failure(AppError.dataNotFound))
-            return
-        }
-
-        do {
-           //create json object from data
-           if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
-            let error = json["error"] ?? nil
-            print("TEST 1 json error : ",error)
-            guard error == nil else {
-              let _error = error as! String
-              print("TEST json error : ",error)
-              if (_error == "invalid_token" || _error == "expired_token") {
+      guard error == nil else {
+        completion(Result.failure(AppError.networkError(error!)))
+        return
+      }
+      
+      guard let data = data else {
+        completion(Result.failure(AppError.dataNotFound))
+        return
+      }
+      
+      do {
+        //create json object from data
+        if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+          let error = json["error"] ?? nil
+          guard error == nil else {
+            let _error = error as! String
+            if (_error == "invalid_token" || _error == "expired_token") {
+              self.refreshAccessToken(authData: dataDict) {responseAT in
+                guard responseAT != nil else {
                   completion(Result.failure(AppError.invalidToken))
                   return
+                }
+                return self.callEndPoint(params, completion: completion)
               }
-              completion(Result.failure(AppError.unknownError(_error )))
+              completion(Result.failure(AppError.invalidToken))
               return
             }
-            print("TEST json : ",json)
-            completion(Result.success(json))
+            completion(Result.failure(AppError.unknownError(_error )))
             return
-           }
-        } catch let error {
-            print("TEST error : ",error.localizedDescription)
-            completion(Result.failure(AppError.jsonParsingError(error)))
-            return
+          }
+          completion(Result.success(json))
+          return
         }
+      } catch let error {
+        completion(Result.failure(AppError.jsonParsingError(error)))
         return
+      }
+      return
     }
     task.resume()
   }
   
-  func refreshAccessToken(data: Dictionary<String?, Any>?, completion: @escaping (Any?) -> Void) {
-    
-    let clientId = data!["clientId"] as! String?
-    let clientSecret = data!["clientSecret"] as! String?
-    let refreshToken = data!["refreshToken"] as! String?
+  func refreshAccessToken(authData: Dictionary<String?, Any>?, completion: @escaping (Any?) -> Void) {
+    let clientId = authData!["clientId"] as! String?
+    let clientSecret = authData!["clientSecret"] as! String?
+    let refreshToken = authData!["refreshToken"] as! String?
     guard clientId != nil, clientSecret != nil, refreshToken != nil else {
-        completion(nil)
-        return
+      completion(nil)
+      return
     }
     
     let url = URL(string: "\(Constants.telldusAPIServer)/oauth2/accessToken")!
@@ -115,41 +114,44 @@ class API {
       "refresh_token": refreshToken,
     ]
     do {
-        request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+      request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
     } catch _ {
       completion(nil)
       return
     }
-    
-    print("TEST url \(url)")
+
     let task = URLSession.shared.dataTask(with: request) {(data, response, error) in
-        guard error == nil else {
-            completion(nil)
-            return
-        }
-
-        guard let data = data else {
-            completion(nil)
-            return
-        }
-
-        do {
-           //create json object from data
-           if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
-              guard (json["error"] as? [String]) != nil else {
-                  completion(nil)
-                  return
-              }
-              print("TEST json : ",json)
-              completion(json)
-              return
-           }
-        } catch let error {
-            print("TEST error : ",error.localizedDescription)
-            completion(nil)
-            return
-        }
+      guard error == nil else {
+        completion(nil)
         return
+      }
+      
+      guard let data = data else {
+        completion(nil)
+        return
+      }
+      
+      do {
+        //create json object from data
+        if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+          guard (json["error"] as? [String]) == nil else {
+            completion(nil)
+            return
+          }
+          let accessToken = json["access_token"]
+          guard accessToken != nil else {
+            completion(nil)
+            return
+          }
+          let secData: Dictionary = authData!;
+          let _ = WidgetModule().updateSecureData(data: Utilities().convertDictionaryToString(dict: secData));
+          completion(json)
+          return
+        }
+      } catch let error {
+        completion(nil)
+        return
+      }
     }
     task.resume()
   }
