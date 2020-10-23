dcdb1b43965197e5c2e5a1f91236f4d1b197a717
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2020-10-23 11:29:31 +0530

    Show option to choose sensor update interval
    See #1036.

---
+++ ios/HomescreenWidget/UIViews/SensorWidget/SensorClass.swift
@@ -0,0 +1,39 @@
+//
+//  SensorClass.swift
+//  TelldusLiveApp
+//
+//  Created by Rimnesh Fernandez on 23/10/20.
+//  Copyright © 2020 Telldus Technologies AB. All rights reserved.
+//
+
+import Foundation
+
+class SensorClass {
+  static let intervalOne: Dictionary<String, Any> = [
+    "id": "1",
+    "label": "Update every 5 minutes",
+    "valueInMin": 5,
+  ]
+  static let intervalTwo: Dictionary<String, Any> = [
+    "id": "2",
+    "label": "Update every 10 minutes",
+    "valueInMin": 10,
+  ]
+  static let intervalThree: Dictionary<String, Any> = [
+    "id": "3",
+    "label": "Update every 30 minutes",
+    "valueInMin": 30,
+  ]
+  static let intervalFour: Dictionary<String, Any> = [
+    "id": "4",
+    "label": "Update every 1 hour",
+    "valueInMin": 60,
+  ]
+  static let SensorUpdateInterval: [Dictionary<String, Any>] = [
+    intervalOne,
+    intervalTwo,
+    intervalThree,
+    intervalFour
+  ]
+}
+
