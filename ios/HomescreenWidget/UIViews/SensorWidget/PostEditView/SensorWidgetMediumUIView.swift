9b227fef08c7edf7a04b6405b3c0f7e342755b6c
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2020-11-10 10:43:06 +0530

    Update sensor widget ui - medium
    See #1036.

--- ios/HomescreenWidget/UIViews/SensorWidget/PostEditView/SensorWidgetMediumUIView.swift
+++ ios/HomescreenWidget/UIViews/SensorWidget/PostEditView/SensorWidgetMediumUIView.swift
@@ -12,19 +12,47 @@ struct SensorWidgetMediumUIView: View {
   let sensorWidgetStructure: SensorWidgetStructure
   var body: some View {
     let name = sensorWidgetStructure.name
-    ZStack {
-      Color(UIColor.systemIndigo)
-      VStack {
-        Text("Medium Widget")
-          .font(.headline)
-          .foregroundColor(.white)
-        Text(name)
-          .font(.caption)
-          .multilineTextAlignment(.center)
-          .padding(.top, 5)
-          .padding([.leading, .trailing])
-          .foregroundColor(.white)
+    let icon = sensorWidgetStructure.icon
+    let value = sensorWidgetStructure.value
+    let unit = sensorWidgetStructure.unit
+    let luTime = SensorUtilities().getLastUpdatedString(lastUpdated: sensorWidgetStructure.luTime)
+    ZStack(alignment: .center) {
+      Color("widgetBackgroundColor")
+      VStack (alignment: .center) {
+        ZStack(alignment: .center) {
+          ContainerRelativeShape()
+            .fill(Color("brandPrimary"))
+          HStack (alignment: .center) {
+            Text(icon)
+              .foregroundColor(.white)
+              .font(.custom("telldusicons", size: 42))
+            VStack (alignment: .leading) {
+              HStack {
+                Text(value)
+                  .foregroundColor(.white)
+                  .font(.system(size: 22))
+                  .lineLimit(1)
+                Text(unit)
+                  .foregroundColor(.white)
+                  .font(.system(size: 20))
+              }
+              Text(luTime)
+                .foregroundColor(SensorUtilities().isTooOld(lastUpdated: sensorWidgetStructure.luTime) ? .red : .white)
+                .font(.system(size: 16))
+            }
+          }
+        }
+        HStack (alignment: .bottom) {
+          Text(name)
+            .multilineTextAlignment(.center)
+            .padding(.top, 5)
+            .padding([.leading, .trailing])
+            .foregroundColor(.white)
+            .lineLimit(1)
+            .font(.system(size: 15))
+        }
       }
+      .padding(5)
     }
   }
 }
