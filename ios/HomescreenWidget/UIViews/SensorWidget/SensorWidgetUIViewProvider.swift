882b0d057d87532840994a4b17fa956e32b3bf45
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2020-11-11 10:56:00 +0530

    Remove medium,large sensor widget UI Views - iOS
    See #1036.

--- ios/HomescreenWidget/UIViews/SensorWidget/SensorWidgetUIViewProvider.swift
+++ ios/HomescreenWidget/UIViews/SensorWidget/SensorWidgetUIViewProvider.swift
@@ -18,62 +18,17 @@ struct SensorWidgetUIViewProvider: View {
   var body: some View {
     switch sensorWidgetStructure.displayType {
     case .preview:
-      switch family {
-      case .systemSmall:
         return AnyView(SWPreviewSmallUIView())
-      case .systemMedium:
-        return AnyView(SWPreviewMediumUIView())
-      case .systemLarge:
-        return AnyView(SWPreviewLargeUIView())
-      default:
-        return AnyView(SWPreviewSmallUIView())
-      }
     case .preEditView:
       return AnyView(EditWidgetInfoUIView())
     case .postEditView:
-      switch family {
-      case .systemSmall:
         return AnyView(SensorWidgetSmallUIView(sensorWidgetStructure: sensorWidgetStructure))
-      case .systemMedium:
-        return AnyView(SensorWidgetMediumUIView(sensorWidgetStructure: sensorWidgetStructure))
-      case .systemLarge:
-        return AnyView(SensorWidgetLargeUIView(sensorWidgetStructure: sensorWidgetStructure))
-      default:
-        return AnyView(SensorWidgetSmallUIView(sensorWidgetStructure: sensorWidgetStructure))
-      }
     case .notLoggedInView:
-      switch family {
-      case .systemSmall:
         return AnyView(NotLoggedInView())
-      case .systemMedium:
-        return AnyView(NotLoggedInView())
-      case .systemLarge:
-        return AnyView(NotLoggedInView())
-      default:
-        return AnyView(NotLoggedInView())
-      }
     case .notSameAccountView:
-      switch family {
-      case .systemSmall:
         return AnyView(NotSameAccountUIView(owningAccount: sensorWidgetStructure.owningAccount))
-      case .systemMedium:
-        return AnyView(NotSameAccountUIView(owningAccount: sensorWidgetStructure.owningAccount))
-      case .systemLarge:
-        return AnyView(NotSameAccountUIView(owningAccount: sensorWidgetStructure.owningAccount))
-      default:
-        return AnyView(NotSameAccountUIView(owningAccount: sensorWidgetStructure.owningAccount))
-      }
     case .upgradeToPremiumView:
-      switch family {
-      case .systemSmall:
         return AnyView(SensorWidgetSmallUIView(sensorWidgetStructure: sensorWidgetStructure))
-      case .systemMedium:
-        return AnyView(SensorWidgetMediumUIView(sensorWidgetStructure: sensorWidgetStructure))
-      case .systemLarge:
-        return AnyView(SensorWidgetLargeUIView(sensorWidgetStructure: sensorWidgetStructure))
-      default:
-        return AnyView(SensorWidgetSmallUIView(sensorWidgetStructure: sensorWidgetStructure))
-      }
     }
   }
 }
