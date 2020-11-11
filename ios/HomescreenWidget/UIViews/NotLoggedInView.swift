aa4160c32fca68c54e31bcfd6970cceefa7b0cc5
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2020-11-11 20:27:30 +0530

    Update widget UI - not logged in view
    See #1036.

--- ios/HomescreenWidget/UIViews/NotLoggedInView.swift
+++ ios/HomescreenWidget/UIViews/NotLoggedInView.swift
@@ -12,16 +12,18 @@ struct NotLoggedInView: View {
   
   @available(iOS 13.0.0, *)
   var body: some View {
-    ZStack {
-      Color(UIColor.systemIndigo)
-      VStack {
-        Text("Please log into the app to add widgets")
-          .font(.headline)
-          .multilineTextAlignment(.center)
+    VStack (alignment: .center, spacing: 0) {
+      Text("\u{e92e}")
+        .foregroundColor(Color("widgetTextColorOne"))
+        .font(.custom("telldusicons", size: 32))
+        Text("Please log into the app to add widgets.")
+          .foregroundColor(Color("widgetTextColorOne"))
+          .font(.system(size: 16))
           .padding(.top, 5)
-          .padding([.leading, .trailing])
-          .foregroundColor(.white)
-      }
+          .multilineTextAlignment(.center)
     }
+    .padding(.all, 8)
+    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+    .background(Color("widgetTopBGC"))
   }
 }
