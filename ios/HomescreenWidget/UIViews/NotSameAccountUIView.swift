51c8e487b2875ea731b56500cd1a02a764df9e25
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2020-11-11 20:27:19 +0530

    Update widget UI - not active account view
    See #1036.

--- ios/HomescreenWidget/UIViews/NotSameAccountUIView.swift
+++ ios/HomescreenWidget/UIViews/NotSameAccountUIView.swift
@@ -12,16 +12,18 @@ struct NotSameAccountUIView: View {
   let owningAccount: String
   @available(iOS 13.0.0, *)
   var body: some View {
-    ZStack {
-      Color(UIColor.systemIndigo)
-      VStack {
+    VStack (alignment: .center, spacing: 0) {
+      Text("\u{e92e}")
+        .foregroundColor(Color("widgetTextColorOne"))
+        .font(.custom("telldusicons", size: 30))
         Text("This widget was added for \(owningAccount). Please log in to that account to view this widget.")
-          .font(.headline)
-          .multilineTextAlignment(.center)
+          .foregroundColor(Color("widgetTextColorOne"))
+          .font(.system(size: 13))
           .padding(.top, 5)
-          .padding([.leading, .trailing])
-          .foregroundColor(.white)
-      }
+          .multilineTextAlignment(.center)
     }
+    .padding(.all, 8)
+    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+    .background(Color("widgetTopBGC"))
   }
 }
