c0978e2ea400b6f23ce8f249447d07c684909f90
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2020-11-26 19:10:55 +0530

    Use updated intl string

--- js/App/Components/Dashboard/SetNameMetWeather.js
+++ js/App/Components/Dashboard/SetNameMetWeather.js
@@ -87,7 +87,7 @@ const SetNameMetWeather = memo<Object>((props: Object): Object => {
 	const { layout } = useSelector((state: Object): Object => state.app);
 
 	useEffect(() => {
-		onDidMount(formatMessage(i18n.setName));
+		onDidMount(formatMessage(i18n.selectAName));
 	}, [formatMessage, onDidMount]);
 
 	const {
